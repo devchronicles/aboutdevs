1e0570ab1fb4cf9e160d210a7b066801f36ecd74
andrerpena <andrerpena@gmail.com>
2017-03-03 00:39:56 -0300

    Better looking

--- src/client/components/Hero.js
+++ src/client/components/Hero.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Hero = () => (
-    <div className="header">
-        <a className="logo">
-            INDIE JOBS
-        </a>
+    <div className="hero">
+        <h1>Conectando profissionais a quem precisa de serviços</h1>
+        <h2>Do que você precisa hoje?</h2>
     </div>
 );
 
--- src/client/pages/IndexPage.js
+++ src/client/pages/IndexPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
+import Hero from '../components/Hero';
 
 const IndexPage = () => (
-    <div>
-        
-    </div>
+    <Hero />
 );
 
 export default IndexPage;
