25fc4fb10040527598edddfa0521b0ae637c5d76
andrerpena <andrerpena@gmail.com>
2017-03-04 20:27:02 -0300

    Login page done

--- src/client/pages/LoginPage.js
+++ src/client/pages/LoginPage.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+
+const SocialButton = ({ text, url, faClass }) => (
+    <a className="social-button" href={url}>
+        <i className={`fa fa-${faClass}`} aria-hidden="true" />
+        <span className="text">
+            {text}
+        </span>
+    </a>
+);
+
+SocialButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    faClass: PropTypes.string.isRequired
+};
 
 const Login = () => (
     <div className="login-page-wrapper">
@@ -7,7 +22,9 @@ const Login = () => (
                 Entrar no IndieJobs
             </div>
             <div className="body">
-                
+                <SocialButton text="Entrar com o Google" url="#" faClass="google" />
+                <SocialButton text="Entrar com o Facebook" url="#" faClass="facebook" />
+                <SocialButton text="Entrar com o LinkedIn" url="#" faClass="linkedin" />
             </div>
         </div>
     </div>
