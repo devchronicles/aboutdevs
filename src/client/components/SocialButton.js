fc1153a14ee3cad7a5d024d5fe870947de6d0a35
andrerpena@gmail.com <andrerpena@gmail.com>
2017-04-22 12:00:30 -0300

    Updating lots of packages and adding the SocialButton as a component

--- src/client/components/SocialButton.js
+++ src/client/components/SocialButton.js
@@ -1,13 +1,37 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const SocialButton = ({ text, url, faClass }) => (
-    <a className="social-button" href={url}>
-        <i className={`fa fa-${faClass}`} aria-hidden="true" />
-        <span className="text">
-            {text}
-        </span>
-    </a>
-);
+class SocialButton extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleLinkClick = this.handleLinkClick.bind(this);
+        this.state = {
+            loading: false
+        };
+    }
+
+    handleLinkClick(e) {
+        if (this.state.loading) {
+            e.preventDefault();
+        } else {
+            this.setState({ loading: true });
+        }
+    }
+
+    render() {
+        const { url, faClass, text } = this.props;
+
+        const finalText = this.state.loading ? '...' : text;
+
+
+        return (<a className="social-button" href={url} onClick={this.handleLinkClick} >
+            <i className={`fa fa-${faClass}`} aria-hidden="true" />
+            <span className="text">
+                {finalText}
+            </span>
+        </a>);
+    }
+}
 
 SocialButton.propTypes = {
     text: PropTypes.string.isRequired,
