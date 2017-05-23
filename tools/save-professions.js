fba7ee56c2524e3ebe0bd432aa2a6378af8c0cec
andrerpena <andrerpena@gmail.com>
2017-05-23 18:37:38 -0300

    Fixing script to save professions

--- tools/save-professions.js
+++ tools/save-professions.js
@@ -1,15 +1,17 @@
 import { v4 } from 'uuid';
 import config from '../config/config';
 import { buildMassive } from '../src/server/helpers/massiveHelper';
-import professions from '../data/professions.json';
+import professions from '../data/professions-processed.json';
 
 
 const db = buildMassive(config.db.connectionString);
 
 professions.data.forEach((profession) => {
     console.log(`Saving profession: ${profession}`);
-    db.profession.insert({
+    db.profession.insertSync({
         id: v4(),
-        name_canonical: profession
+        name_canonical: profession[0],
+        name_feminine: profession[1]
     });
 });
+process.exit(0);
