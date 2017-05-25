b3544d56022ad1d94d8255047c3ebe4ccd8dd814
andrerpena <andrerpena@gmail.com>
2017-05-24 21:46:48 -0300

    Front page not looking ridiculous

---
+++ src/client/components/IndexHero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const IndexHero = () => (
+    <span className="index-hero">
+        <div className="icon-box">
+            <i className="fa fa-briefcase" aria-hidden="true" />
+        </div>
+        <div className="hero-text">
+            <div className="hero-logo">IndieJobs</div>
+            <div className="hero-motto">do que você precisa hoje?</div>
+        </div>
+    </span>
+);
+
+export default IndexHero;
--- src/client/components/IndexSearchForm.js
+++ src/client/components/IndexSearchForm.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import PropTypes from 'prop-types';
 import { SelectLocation, FormRow, FormColumn, FormGroup } from './form/index';
+import IndexHero from './IndexHero';
 
 let IndexSearchForm = (props) => {
     const { handleSubmit } = props;
     return (<div className="index-search-form">
-        <form onSubmit={handleSubmit}>
+        <div className="logo-wrapper">
+            <IndexHero />
+        </div>
+        <form onSubmit={handleSubmit} className="flex-column flex-align-items-center ">
             <FormRow>
                 <FormGroup>
                     <Field
@@ -30,7 +34,7 @@ let IndexSearchForm = (props) => {
             </FormRow>
             <button type="submit" className="vibrant">
                 <i className="fa fa-search" aria-hidden="true" />
-                <span>Pesquisar</span>
+                <span>Encontrar profissionais</span>
             </button>
         </form>
     </div>);
