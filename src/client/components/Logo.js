208867fc5162c45e6057e5cd7c8bcaee7148a2e4
andrerpena <andrerpena@gmail.com>
2017-05-24 23:22:30 -0300

    Adding some background options

--- src/client/components/Logo.js
+++ src/client/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => (
     <span className="logo">
         <i className="fa fa-briefcase" aria-hidden="true" />
-        <span>INDIE JOBS</span>
+        <span>IndieJobs</span>
     </span>
 );
 
