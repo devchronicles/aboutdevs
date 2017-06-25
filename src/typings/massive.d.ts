8d7b1dad8b23018e4589f57ccda2e9d4839e4a12
André Pena <andrerpena@gmail.com>
2017-06-25 20:09:12 +0100

    Fixing type definitions for massive

---
+++ src/typings/massive.d.ts
@@ -0,0 +1,9 @@
+import * as massive from "massive"
+
+declare module "massive" {
+    export interface Table<T> {
+        save(data: object): Promise<T[] | T>;
+        insert(data: object): Promise<T[] | T>;
+        update(dataOrCriteria: object, changesMap?: object): Promise<T[] | T>;
+    }
+}
