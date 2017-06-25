6fb098b6caabc2658a7bade9894b2e689a5c77b6
André Pena <andrerpena@gmail.com>
2017-06-25 20:08:09 +0100

    Migrating the db tools to TS

--- tools/db-manager.ts
+++ tools/db-manager.ts
@@ -1,12 +1,19 @@
-const exec = require('child_process');
+import * as childProcess from "child_process";
 
 const selectedCommand = process.env.COMMAND;
 const dbName = 'indiejobs';
 const dbTestsName = 'indiejobs_tests';
 const userName = 'indiejobs';
 
-const commands = {
-    create_dev_dbs: `
+/* tslint:disable */
+console.log('===Starting DB Command===');
+console.log(`Executing ${selectedCommand}`);
+/* tslint:enable */
+
+let commands;
+switch (selectedCommand) {
+    case "create_dev_dbs":
+        commands = `
         echo ===Creating the ${dbName} dev db===
         createdb -E UTF8 --lc-collate C --lc-ctype C -U ${userName} -T template0 ${dbName}
         echo ===Creating the ${dbTestsName} dev db===
@@ -15,29 +22,33 @@ const commands = {
         psql -f db/setupDb.sql -U ${userName} -d ${dbName}
         echo ===Setting up the ${dbName} dev db===
         psql -f db/setupDb.sql -U ${userName} -d ${dbTestsName}
-    `,
-    drop_dev_dbs: `
+    `;
+        break;
+    case "drop_dev_dbs":
+        commands = `
         echo ===Dropping the ${dbName} dev db===
         dropdb -U ${userName} ${dbName}
         echo ===Dropping the ${dbTestsName} dev db===
         dropdb -U ${userName} ${dbTestsName}
-    `,
-    clean_up_dev_dbs: `
+    `;
+        break;
+    case "clean_up_dev_dbs":
+        commands = `
         echo ===Cleaning up the ${dbName} dev db===
         psql -f db/cleanUpDb.sql -U ${userName} -d ${dbName}
         echo ===dropping the ${dbTestsName} dev db===
         psql -f db/cleanUpDb.sql -U ${userName} -d ${dbTestsName}
-    `,
-    generate_scripts: `
+    `;
+        break;
+    case "generate_scripts":
+        commands = `
         echo ===Create the setupDb.sql file===
         pg_dump --schema-only -W -w -f db/setupDb.sql -p 5432 -U ${userName} ${dbName}
-    `
-};
+        `;
+        break;
+}
 
-console.log('===Starting DB Command===');
-console.log(`Executing ${selectedCommand}`);
-
-commands[selectedCommand].split('\n').forEach((command) => {
-    console.log(exec.execSync(command).toString());
+commands.split('\n').forEach((command: string) => {
+    console.log(command);
+    childProcess.execSync(command);
 });
-
