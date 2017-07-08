e4c18fcc765802fcaea3207de3317367cb28edb9
André Pena <andrerpena@gmail.com>
2017-07-08 13:17:07 -0300

    Migrating httpClient to TypeScript

--- src/client/httpClient.ts
+++ src/client/httpClient.ts
@@ -1,25 +1,26 @@
 import axios from 'axios';
+import * as commonTypes from '../common/typings';
 
 export function getProfileData() {
     return axios.get('/api/users/myprofile');
 }
 
-export function saveProfileData(profile) {
+export function saveProfileData(profile: commonTypes.IReduxUserProfile) {
     if (!profile) throw Error('Argument \'profile\' should be truthy');
     return axios.post('/api/users/myprofile', profile);
 }
 
-export function getFormattedLocations(searchTerm, allowCities = false) {
+export function getFormattedLocations(searchTerm: string, allowCities = false) {
     const queryString = allowCities ? `q=${searchTerm}&allowcities=true` : `q=${searchTerm}`;
     return axios.get(`/api/address?${queryString}`);
 }
 
-export function getProfessions(searchTerm) {
+export function getProfessions(searchTerm: string) {
     const queryString = `q=${searchTerm}`;
     return axios.get(`/api/professions?${queryString}`);
 }
 
-export function checkUserName(userName) {
+export function checkUserName(userName: string) {
     if (!userName) throw Error('Argument \'userName\' should be truthy');
     const queryString = `q=${userName}`;
     return axios.get(`/api/users/checkname?${queryString}`);
