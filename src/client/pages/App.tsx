ade16a77173465d8c8c698732b98078d4f97d222
André Pena <andrerpena@gmail.com>
2017-07-09 22:23:35 -0300

    Some linting

--- src/client/pages/App.tsx
+++ src/client/pages/App.tsx
@@ -11,7 +11,7 @@ import * as ReactRedux from 'react-redux';
 import * as ReactRouter from 'react-router';
 
 interface AppStateProps {
-    loggedUser: commonTypes.ReduxCurrentUserProfile
+    loggedUser: commonTypes.ReduxCurrentUserProfile,
 }
 
 interface AppDispatchProps {
@@ -28,9 +28,9 @@ const App: React.SFC<AppProps> = ({ loggedUser }) => (
     <div className="container">
         <Header loggedUser={loggedUser} />
         <Switch>
-            <Route exact path="/search" component={SearchPage} />
-            <Route exact path="/config/edituserprofile" component={ProfileEditPage} />
-            <Route exact path="/" component={IndexPage} />
+            <Route exact={true} path="/search" component={SearchPage} />
+            <Route exact={true} path="/config/edituserprofile" component={ProfileEditPage} />
+            <Route exact={true} path="/" component={IndexPage} />
         </Switch>
     </div>
 );
@@ -38,7 +38,7 @@ const App: React.SFC<AppProps> = ({ loggedUser }) => (
 // CONNECT
 
 const mapStateToProps = (state: clientTypes.ReduxState): AppStateProps => ({
-    loggedUser: state.loggedUser
+    loggedUser: state.loggedUser,
 });
 
 const mapDispatchToProps = (dispatch: ReactRedux.Dispatch<clientTypes.ReduxState>): AppDispatchProps => ({
@@ -48,14 +48,13 @@ const mapDispatchToProps = (dispatch: ReactRedux.Dispatch<clientTypes.ReduxState
 const mergeProps = (stateProps: AppStateProps, dispatchProps: AppDispatchProps, ownProps: AppOwnProps): AppProps => ({
     ...stateProps,
     ...dispatchProps,
-    ...ownProps
+    ...ownProps,
 });
 
 const ConnectedApp = connect<AppStateProps, AppDispatchProps, AppOwnProps, AppProps>(
     mapStateToProps,
     mapDispatchToProps,
-    mergeProps
+    mergeProps,
 )(App);
-
 
 export { ConnectedApp as App }
