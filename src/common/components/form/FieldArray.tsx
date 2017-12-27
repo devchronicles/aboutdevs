5fba3f1c148bbfd47afbf0a6c6330acd218d706e
André Pena <andrerpena@gmail.com>
2017-12-27 13:48:13 -0200

    Making the FieldArrays to be able to have validation too

--- src/common/components/form/FieldArray.tsx
+++ src/common/components/form/FieldArray.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { BaseFieldsProps, FieldArray as RfFieldArray } from "redux-form";
+import * as fieldValidationHelper from "../../helpers/fieldValidationHelper";
 
 interface FieldProps extends BaseFieldsProps {
 }
@@ -7,7 +8,7 @@ interface FieldProps extends BaseFieldsProps {
 declare type IFinalFieldProps = FieldProps & any;
 
 const FieldArray: React.SFC<IFinalFieldProps> = (props) => (
-    <RfFieldArray {...props} />
+    <RfFieldArray {...props} validate={props.validate || fieldValidationHelper.getValidatorsForField(props.name)}/>
 );
 
 export { FieldArray };
