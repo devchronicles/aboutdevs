b78a45fdb7a688bee535f995e6b2ed56457c5e07
André Pena <andrerpena@gmail.com>
2018-01-05 00:17:47 -0200

    Faker now works

--- tools/fake.ts
+++ tools/fake.ts
@@ -1,110 +1,139 @@
-// import * as faker from 'faker/locale/pt_BR';
-//
-// import buildDb from '../src/server/db/buildDb';
-//
-// import * as userService from '../src/server/services/userService';
-// import * as locationService from '../src/server/services/locationService';
-// import * as commonTypes from '../src/common/typings';
-// import * as serverTypes from '../src/server/typings';
-// import * as stringHelper from '../src/common/helpers/stringHelper';
-//
-// buildDb()
-//     .then(async db => {
-//
-//         // Make sure the default
-//
-//         for (let i = 0; i < 50000; i++) {
-//             try {
-//                 const displayName = faker.name.findName();
-//
-//                 let name = await userService.getValidUserName(db, stringHelper.normalizeForUrl(faker.internet.userName()));
-//                 name += i;
-//                 const email = `${name}@gmail.com`;
-//
-//                 const preUserProfile = {
-//                     display_name: displayName,
-//                     email: email,
-//                     gender: faker.random.number({min: 0, max: 1}),
-//                     name: name,
-//                     oauth_profiles: {},
-//                     photo_url: faker.internet.avatar(),
-//                 };
-//
-//                 const insertedUser = (await db.user.insert(preUserProfile)) as serverTypes.User;
-//
-//                 const fakeProfile: commonTypes.UserProfile = {
-//                     id: insertedUser.id,
-//                     name: name,
-//                     displayName: displayName,
-//                     type: commonTypes.UserProfileType.RECRUITER,
-//                     bio: faker.lorem.sentences(3),
-//                     activities: faker.lorem.sentences(3),
-//                     phoneWhatsapp: faker.phone.phoneNumber("(##) 9####-####"),
-//                     phoneAlternative: faker.phone.phoneNumber("(##) 9####-####"),
-//                     status: commonTypes.UserProfileStatus.READY,
-//                     // These are just so it will compile
-//                     title: '',
-//                     address: 'Rua Henrique Surerus 28, Juiz de Fora MG',
-//                     services: [
-//                         {
-//                             index: 0,
-//                             name: faker.lorem.sentence(4).substring(0, 60)
-//                         },
-//                         {
-//                             index: 2,
-//                             name: faker.lorem.sentence(4).substring(0, 60)
-//                         },
-//                         {
-//                             index: 3,
-//                             name: faker.lorem.sentence(4).substring(0, 60)
-//                         }
-//                     ]
-//                 };
-//
-//                 let randomProfessionId;
-//                 let randomProfessionText;
-//                 try {
-//                     const randomProfession = (await db.get_random_profession())[0];
-//                     randomProfessionId = randomProfession.id;
-//                     randomProfessionText = randomProfession.name_canonical;
-//                 } catch (ex) {
-//                     console.error('Error getting random title id. Are you sure you have populated the database?');
-//                     process.exit();
-//                 }
-//
-//                 // location
-//                 const country = await locationService.saveCountry(db, 'BR', 'Brazil');
-//                 const state = await locationService.saveState(db, 'MG', 'Minas Gerais', country.id);
-//                 const city = await locationService.saveCity(db, 'Juiz de Fora', state.id);
-//
-//                 const neighborhood = 'Pavuna';
-//                 const formattedAddress = `${faker.address.streetName()}, ${neighborhood}, ${faker.city.city()}`;
-//
-//                 const maxLat = -21.3743555;
-//                 const minLat = -22.3248544;
-//
-//                 const minLong = -51.5182245;
-//                 const maxLong = -50.2197401;
-//
-//                 const randomLat = Math.random() * (maxLat - minLat) + minLat;
-//                 const randomLong = Math.random() * (maxLong - minLong) + minLong;
-//
-//                 let location = await db.geo_location.findOne({formatted_address: formattedAddress});
-//
-//                 location = location
-//                     ? location
-//                     : await locationService.saveLocation(db, formattedAddress, neighborhood, city.id, randomLat, randomLong);
-//
-//                 fakeProfile.title = randomProfessionText;
-//
-//                 await userService.saveProfile(db, insertedUser.id, fakeProfile, randomProfessionId, location.id);
-//             }
-//             catch (ex) {
-//                 console.log('error');
-//                 throw ex;
-//             }
-//             console.log('Person saved ' + i);
-//         }
-//
-//         process.exit();
-//     });
+import * as faker from "faker";
+
+import buildDb from "../src/server/db/buildDb";
+
+import * as userService from "../src/server/services/userService";
+import * as serverTypes from "../src/server/typings";
+import * as stringHelper from "../src/common/helpers/stringHelper";
+import { UserProfileStatus, UserProfileType } from "../src/common/typings";
+import { getAndSaveCity, searchCitiesFormatted } from "../src/server/services/googlePlacesService";
+
+const randomTags = [
+    // .NET
+    "c#",
+    ".net",
+    "entity-framework",
+    "asp.net",
+    // JavaScript
+    "javascript",
+    "typescript",
+    "es2015",
+    "react",
+    "angular",
+    "vue",
+    // Data science
+    "machine-learning",
+    "tensorflow",
+    "matplotlib",
+    "pandas",
+    "python",
+    // Languages
+    "ruby-on-rails",
+    "pascal",
+    "go",
+    "ruby",
+    "kotlin",
+];
+
+const randomCities = [
+    // North America
+    "New York",
+    "Seattle",
+    "Boston",
+    "Los Angeles",
+    // Latin America
+    "Mexico City",
+    "Sao Paulo",
+    "Rio de Janeiro",
+    "Buenos Aires",
+    // Europe
+    "London",
+    "Berlin",
+    "Amsterdam",
+    "Moscow",
+    "Zurich",
+    "Oslo",
+    "Dublin",
+    // Asia
+    "Tokyo",
+    "Hong Kong",
+    "New Delhi",
+];
+
+function getRandomArrayItem(array: string[]) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+buildDb()
+    .then(async db => {
+        for (let i = 0; i < 5000; i++) {
+            try {
+                let userName = await userService.getValidUserName(db, stringHelper.normalizeForUrl(faker.internet.userName()));
+                userName += i;
+                const email = `${userName}@gmail.com`;
+
+                const user: serverTypes.User = {
+                    bio: faker.lorem.paragraphs(3, "\n\n"),
+                    status: UserProfileStatus.PENDING_PROFILE_ACTIVATION,
+                    type: UserProfileType.DEVELOPER,
+                    display_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+                    company_name: faker.company.companyName(),
+                    email,
+                    name: userName,
+                    title: faker.name.jobTitle(),
+                    oauth_profiles: {
+                        linkedin: {
+                            id: "1",
+                            raw: {},
+                        },
+                    },
+                    social_links: {
+                        socialLinks: [{
+                            website: "linkedin",
+                            url: "http://www.linkedin.com",
+                        }],
+                    },
+                    info_groups: null,
+                    photo_url: faker.image.avatar(),
+                    colors: {
+                        headerBackground: "#252934",
+                        headerText: "#FFFFFF",
+                        bodyBackground: "#3073b5",
+                        bodyText: "#FFFFFF",
+                    },
+                    tags: "",
+                };
+
+                // Location
+                let userLocation = getRandomArrayItem(randomCities);
+                if (userLocation) {
+                    userLocation = userLocation.replace(" Area", "");
+                    const citiesFormatted = await searchCitiesFormatted(db, userLocation);
+                    if (citiesFormatted && citiesFormatted.length) {
+                        const googlePlace = await getAndSaveCity(db, citiesFormatted[0]);
+                        user.google_place_id = googlePlace.id;
+                        user.google_place_formatted_address = googlePlace.formatted_address;
+                    }
+                }
+
+                const insertedUser = (await db.user.insert(user)) as serverTypes.User;
+
+
+                const userProfile = await userService.getUserProfile(db, insertedUser);
+                userProfile.tags = [getRandomArrayItem(randomTags), getRandomArrayItem(randomTags), getRandomArrayItem(randomTags)];
+
+                await userService.saveProfile(db, insertedUser.id, userProfile);
+            }
+            catch (ex) {
+                console.log("error");
+                throw ex;
+            }
+            console.log("Person saved " + i);
+        }
+
+
+    })
+    .then(() => {
+        process.exit();
+    })
+    .catch((error) => console.log(error));
