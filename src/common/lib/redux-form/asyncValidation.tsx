c0ebd3e61d2090fe21d015eda32e9c074509ded1
André Pena <andrerpena@gmail.com>
2018-01-16 21:48:43 -0200

    Fixing the validation problem with empty user names

--- src/common/lib/redux-form/asyncValidation.tsx
+++ src/common/lib/redux-form/asyncValidation.tsx
@@ -3,10 +3,13 @@ import * as fieldValidation from "../../../common/helpers/fieldValidationHelper"
 import * as commonTypes from "../../../common/typings";
 
 export default function (values: commonTypes.UserProfile) {
+    if (values.name === null || values.name === undefined || values.name === "") {
+        return Promise.resolve(undefined);
+    }
     return httpClient.checkUserName(values.name)
         .then((r) => {
             if (r.data.exists) {
-                throw { name: fieldValidation.USER_NAME_IS_TAKEN };
+                throw {name: fieldValidation.USER_NAME_IS_TAKEN};
             }
         });
 }
