d8b256d86e8a5d1ba2693d906e0e6a8671197d5f
André Pena <andrerpena@gmail.com>
2018-01-18 10:05:29 -0200

    Addint a draft of the Footer component and adding DropZone

---
+++ src/common/components/form/Footer.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+interface FooterProps {
+}
+
+export const Footer: React.SFC<FooterProps> = (props) => {
+    return (
+        <div className="footer">
+            <span className="mark">
+                <span className="trademark">©</span>
+                <a href="https://aboutdevs.com">aboutdevs.com</a>
+            </span>
+            <span className="social">
+                <a href="https://www.reddit.com/r/AboutDevs/">
+                    <i className="fa fa-reddit" aria-hidden="true"/>
+                </a>
+                <a href="https://twitter.com/aboutdevs">
+                    <i className="fa fa-twitter" aria-hidden="true"/>
+                </a>
+                <a href="https://www.facebook.com/aboutdevs/">
+                    <i className="fa fa-facebook" aria-hidden="true"/>
+                </a>
+            </span>
+        </div>
+    );
+};
