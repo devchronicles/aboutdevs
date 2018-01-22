c6d73df04ca68c07ba19cd91c1cf14b959e1d71d
André Pena <andrerpena@gmail.com>
2018-01-22 13:53:52 -0200

    Changing a problem in the title

--- src/common/helpers/pageTitleHelper.ts
+++ src/common/helpers/pageTitleHelper.ts
@@ -1,13 +1,13 @@
-const suffix = " ᛫ AboutDevs";
+const suffix = " - AboutDevs";
 
 export function getPageTitleDefault() {
-    return "AboutDevs ᛫ The Software Developers Database";
+    return "AboutDevs - The Software Developers Database";
 }
 
 export function getPageTitleForProfile(developerName: string): string {
-    return `${developerName} ᛫ AboutDevs`;
+    return `${developerName} - AboutDevs`;
 }
 
 export function getPageTitleSearch() {
-    return `Discover Developers ᛫ AboutDevs`;
+    return `Discover Developers - AboutDevs`;
 }
