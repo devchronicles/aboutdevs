b2127ccdd1611c4355c8c9aa2f535dbf7f5aa5f5
André Pena <andrerpena@gmail.com>
2018-02-13 07:32:03 +0100

    Removing the reference to react-mde

--- src/common/components/form/MarkdownEditor.tsx
+++ src/common/components/form/MarkdownEditor.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import ReactMde, { ReactMdeCommands, ReactMdeTypes } from "react-mde";
 import * as ReduxForm from "redux-form";
 
 interface MarkdownEditorProps extends ReduxForm.WrappedFieldProps {
