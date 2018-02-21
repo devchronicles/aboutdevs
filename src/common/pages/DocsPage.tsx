eb5dff7de94ef440f2ec6f467b97ba4d1625fbc0
André Pena <andrerpena@gmail.com>
2018-02-21 22:20:24 +0100

    Adding the youtube video

--- src/common/pages/DocsPage.tsx
+++ src/common/pages/DocsPage.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Markdown } from "../components/Markdown";
 import { getDocsMarkdown } from "../docs/docsMarkdown";
 import { Logo } from "../components/Logo";
+import YouTube from "react-youtube";
 
 interface DocsPageProps {
 
@@ -17,6 +18,15 @@ export class DocsPage extends React.Component<DocsPageProps, DocsPageState> {
             <div className="page-wrapper">
                 <div className="docs-page-wrapper">
                     <Logo/>
+                    <div className="youtube-container">
+                        <YouTube
+                            videoId={"AKfc_RTBU6A"}
+                            opts={{
+                                width: "100%",
+                                height: "550px",
+                            }}
+                        />
+                    </div>
                     <Markdown markdown={getDocsMarkdown()}/>
                 </div>
             </div>
